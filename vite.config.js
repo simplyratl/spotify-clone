5315d18963097e6a6562f462c912e48580bd78b9
simplyratl <nikicaraznatovic123@gmail.com>
2023-09-10 01:50:59 +0200

    fix dev proxy

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,32 @@
+/* eslint-disable no-undef */
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dotenv from 'dotenv'
+import EnvCompatible from 'vite-plugin-env-compatible'
 
-dotenv.config({ path: `./.env.${process.env.NODE_ENV}` })
+dotenv.config({ path: `./.env` })
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_BASE_URL,
-        changeOrigin: true
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  return defineConfig({
+    plugins: [vue(), EnvCompatible()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_API_BASE_URL,
+          changeOrigin: true
+        }
       }
     }
-  }
-})
+  })
+}
