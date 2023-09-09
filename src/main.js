0ea3348de5233078a394b3955eae64f322850fc2
simplyratl <nikicaraznatovic123@gmail.com>
2023-09-10 01:59:10 +0200

    adding axios interceptor

--- src/main.js
+++ src/main.js
@@ -7,6 +7,21 @@ import App from './App.vue'
 import router from './router'
 
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import axios from 'axios'
+
+axios.interceptors.request.use(
+  function (config) {
+    // Get the base URL from the environment variable
+    config.baseURL = process.env.VUE_APP_BASE_URL
+
+    // You can also add other headers or configuration here
+
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
